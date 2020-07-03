dae49edb9f2c9129cdec5170cfab5cda0f37c168
TheLittleBro122 <mynameiscutepanda@gmail.com>
2020-07-03 19:52:25 +0800

    first commit

---
+++ tchanger.py
@@ -0,0 +1,51 @@
+#Made by TheLittleBro122
+
+import random
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Automate the process of manipulating texts")
+parser.add_argument("-t", "--text", type=str, required=True, help="The text that you want to manipulate")
+
+important_args = parser.add_mutually_exclusive_group()
+important_args.add_argument("-rc", "--randomCapitalization", action="store_true", required=False, help="Randomizes the capitalization")
+important_args.add_argument("-s", "--separate", type=int, required=False, help="Separates the text by number")
+important_args.add_argument("-re", "--reverse", action="store_true", required=False, help="Reverses the text")
+
+args = parser.parse_args()
+
+def randomCapitalization(n):
+    new = ""
+
+    for letter in n:
+        change = random.choice([True, False]);
+
+        if change:
+            new = new + letter.upper()
+        else:
+            new = new + letter.lower()
+
+    return new
+
+def separate(n, space):
+    new = ""
+
+    for letter in n:
+        new = new + letter + " "*space
+
+    return new
+
+def reverse(n):
+    return n[::-1]
+
+if __name__ == "__main__":
+    text = args.text
+
+    if args.randomCapitalization:
+        text = randomCapitalization(text)
+    if args.separate:
+        text = separate(text, args.separate)
+    if args.reverse:
+        text = reverse(text)
+
+    print("\n%s" % (text))
